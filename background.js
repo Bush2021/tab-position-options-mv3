b9bc1da25b65147121735121c885e29675f9b65e
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2025-06-16 23:25:46 -0400

    Reimplement keep-alive
    Kang from https://github.com/AdguardTeam/AdguardBrowserExtension/blob/8ad5a8963fe31c3144641ab96036bb1a03e70b5a/Extension/src/background/keep-alive.ts#L179

--- background.js
+++ background.js
@@ -1,5 +1,56 @@
 let lastActiveTab = null
 
+/**
+ * keep-alive related code are kanged from 
+ * https://github.com/AdguardTeam/AdguardBrowserExtension/blob/8ad5a8963fe31c3144641ab96036bb1a03e70b5a/Extension/src/background/keep-alive.ts#L179
+ */
+const KEEP_ALIVE_PORT_NAME = 'keep-alive'
+
+function setupKeepAlive() {
+    setInterval(() => {
+        chrome.runtime.getPlatformInfo(() => { })
+    }, 20000)
+}
+
+chrome.runtime.onConnect.addListener((port) => {
+    if (port.name === KEEP_ALIVE_PORT_NAME) {
+    }
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url && tab.url.startsWith('http')) {
+        try {
+            chrome.scripting.executeScript({
+                target: { tabId },
+                func: () => {
+                    if (window.keepAliveActive) {
+                        return
+                    }
+
+                    function connect() {
+                        chrome.runtime.connect({ name: 'keep-alive' })
+                            .onDisconnect
+                            .addListener(() => {
+                                connect()
+                            })
+                    }
+
+                    window.addEventListener('pageshow', (event) => {
+                        if (event.persisted) {
+                            connect()
+                        }
+                    })
+
+                    connect()
+                    window.keepAliveActive = true
+                }
+            })
+        } catch (e) {
+        }
+    }
+})
+
+setupKeepAlive()
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.tabs.get(activeInfo.tabId, (tab) => {
